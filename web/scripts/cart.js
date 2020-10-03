c9e4799997e38d65e6a4322cc4ae000e2b8b24e0
Nefi Aguilar <solemnefi153@gmail.com>
2020-10-03 13:46:02 -0600

    Fix cart issues by hiding elements when removed

--- web/scripts/cart.js
+++ web/scripts/cart.js
@@ -1,43 +1,26 @@
 import {addOrRemoveFromTheSessionCart} from './add_romove_cart_items.js';
 
-var unRegisterAllEventListeners = (element) => {
-	if ( typeof element._eventListeners == 'undefined' || element._eventListeners.length == 0 ) {
-    alert('Negron 1');
-		return;	
-  }
-  //store how many listeners
-  var len = element._eventListeners.length;
-	for(var i = 0; i < len; ++i) {
-    alert('Negron 2');
-    //grab one listener object from the element
-    var listener = element._eventListeners[i];
-    //remove the event tistener 
-		element.removeEventListener(listener.event, listener.callback);
-  }
-	obj._eventListeners = [];
-}
-
 var removeItems = button_clicked => {
-  //This prevents users for clicking multiple times atempting to remove an item 
-    button_clicked.style.display = "none";
-    unRegisterAllEventListeners(button_clicked);
-
-    var button_id = button_clicked.id;
-    var query = "itemID=" + button_id;
-    addOrRemoveFromTheSessionCart(query, reloadItemsInCart);
+  //This prevents users for clicking multiple times in the same element atempting to remove an item 
+  button_clicked.parentElement.style.display = "none";
+  
+  //this removes the item from the session
+  var button_id = button_clicked.id;
+  var query = "itemID=" + button_id;
+  addOrRemoveFromTheSessionCart(query, reloadItemsInCart);
 }
 
 var reloadItemsInCart = () => {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-            var content_element = document.getElementById('content');
-            content_element.innerHTML = this.responseText;
-            addEventListeners();
-          }
-        };
-        xhttp.open("GET", "../php/load_items_in_cart.php", true);
-        xhttp.send();
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var content_element = document.getElementById('content');
+      content_element.innerHTML = this.responseText;
+      addEventListeners();
+    }
+  };
+  xhttp.open("GET", "../php/load_items_in_cart.php", true);
+  xhttp.send();
 }
   
   var addEventListeners = () => {
@@ -45,9 +28,8 @@ var reloadItemsInCart = () => {
     remove_btns.forEach(button => {
       button.addEventListener('click', () => {
         removeItems(button);
-      })
+        //This indicates that the listener will only work once
+      }, {once : true});
     })
   }
   addEventListeners();
-
-  
